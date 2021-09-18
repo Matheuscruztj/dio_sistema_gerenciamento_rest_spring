2aa0c47375284980e8ae696601dac3f03ce0d1e4
Matheus Santos <matheus.cbmdk@gmail.com>
2021-09-18 16:49:27 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "ola";
+    }
+}
